eaa576190101b3b0ebc0369238836eb727efc11d
Adarsh Padhi <45957713+adarsh1405@users.noreply.github.com>
2019-08-20 19:51:04 +0530

    enhanced wrt user

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,11 @@
 import json
 data=json.load(open("data.json"))
-wrd=input("ENTER THE WORD :")
-
-if wrd in data:
-    print(data[wrd])
-else:
-    print("WORD NOT EXIST!!")
+c=1
+while c !=0 :
+    wrd=input("ENTER THE WORD :")
+    wrd=wrd.lower()
+    if wrd in data:
+        print(data[wrd])
+    else:
+        print("WORD NOT EXIST!!")
+    c=int(input("ENTER 1 TO SEARCH AGAIN & 0 TO EXIT : "))
