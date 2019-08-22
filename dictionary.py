fe6dfd040ab04579846e59f85cdee72f94807f5e
Adarsh Padhi <45957713+adarsh1405@users.noreply.github.com>
2019-08-22 21:35:47 +0530

    A bit intelligence added to it .

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,18 @@
 import json
+from difflib import get_close_matches
 data=json.load(open("data.json"))
 c=1
 while c !=0 :
-    wrd=input("ENTER THE WORD :")
+    wrd=input("ENTER THE WORD :  ")
     wrd=wrd.lower()
     if wrd in data:
         print(data[wrd])
+    elif len(get_close_matches(wrd,data.keys()))>0:
+        ai=input("Did You Mean %s insted ? Enter Y for YES & N for NO: "%get_close_matches(wrd,data.keys())[0])
+        if ai=="Y" or ai=="y":
+            print(data[get_close_matches(wrd,data.keys())[0]])
+        else:
+            print("Didn't Understand .PLEASE RETRY")
     else:
         print("WORD NOT EXIST!!")
     c=int(input("ENTER 1 TO SEARCH AGAIN & 0 TO EXIT : "))
